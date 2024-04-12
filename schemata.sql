85f450644483a0356777565cec02ab9a44c9bede
thomasjngo <156987041+thomasjngo@users.noreply.github.com>
2024-04-12 10:55:16 -0700

    slight date fix

--- schemata.sql
+++ schemata.sql
@@ -14,11 +14,11 @@ CREATE TABLE titles(
 CREATE TABLE employees(
 	emp_no INT NOT NULL,
 	emp_title_id VARCHAR(30) NOT NULL,
-	birth_date VARCHAR(10) NOT NULL,
+	birth_date DATE NOT NULL,
 	first_name VARCHAR(30) NOT NULL,
 	last_name VARCHAR(30) NOT NULL,
 	sex VARCHAR(1) NOT NULL,
-	hire_date VARCHAR(10) NOT NULL,
+	hire_date DATE NOT NULL,
 	PRIMARY KEY (emp_no),
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
